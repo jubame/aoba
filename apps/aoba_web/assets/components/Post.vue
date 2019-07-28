3e4b89a09d6bd2187289d135d2e1ae1c7518245f
J <j@localhost.localdomain>
2019-07-28 02:37:04 +0200

    Comprobar si hay vídeo antes de hacer pausa, porque a toggleExpand también lo llaman las imágenes.

--- apps/aoba_web/assets/components/Post.vue
+++ apps/aoba_web/assets/components/Post.vue
@@ -342,7 +342,7 @@ export default {
         },
 
         toggleExpand() {
-            this.$refs.video.pause()
+            this.$refs.video && this.$refs.video.pause()
 
             
             return (this.imageExpanded = !this.imageExpanded);
